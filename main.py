e7b139c0b8fb05687435c343b943ae9368e9d44e
Rahmatullin <65865859+Aydar8888@users.noreply.github.com>
2022-03-17 21:27:37 +0300

    Create main.py
    Загрузил main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+from asyncio import sleep
+from itertools import count
+import pyautogui as pag
+import time
+import cv2
+import math
+
+size_matrix_w, size_matrix_h = map(int, input("Введите размер марицы>> ").split())
+
+pag.click(72, 110)  # нажатие по 1 чату в списке
+pag.click(503, 1384) # нажатие по текстовой строке
+time.sleep(3) # задаржка в 3 секнды
+
+
+file = open(r"txt_matrix\ASCII-смайлик.txt", 'r')
+data = file.read()
+y = 1
+x = 0
+for i in data:
+     if i == "0":
+          x += 1
+          pag.typewrite("/set " + str(x)  + " " + str(y))
+          pag.hotkey('enter')  # нажатие на enter
+          time.sleep(0)  # задержка в пол секунды
+               
+     else:
+          x += 1
+          pag.typewrite("/clr " + str(x) + " " + str(y))
+          pag.hotkey('enter')  # нажатие на enter
+          time.sleep(0)  # задержка в пол секунды
+     if x == size_matrix_w:
+          y += 1
+          x = 0
+ 
